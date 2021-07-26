7fb5188f1e76ae57bc13256bdcf258bcc3b06960
phu1204 <73231507+phu1204@users.noreply.github.com>
2021-07-27 01:56:57 +0700

    last update done pj1
    mai bat dau project 2 nhe

---
+++ .history/js/main_20210727012805.js
@@ -0,0 +1,79 @@
+
+function toggleMenu(){
+
+    const menu = document.querySelector(".header-menu");
+    const menuBars = document.querySelector(".header-bar");
+    const menuTimes = document.querySelector(".header-times");
+    if(!menu || !menuBars) return;
+    menuBars && menuBars.addEventListener("click", function(){
+        console.log("hello");
+        menu && menu.classList.add("active");
+    });
+
+    menuBars && menuTimes.addEventListener("click", function(){
+        menu && menu.classList.remove("active");
+    });
+    
+}
+toggleMenu();
+
+//scroll animation
+window.addEventListener('scroll', reveal);  
+function reveal(){
+    var reveals = document.querySelectorAll(".reveal");
+    for(var i = 0 ; i < reveals.length ; i++){
+        var windowheight = window.innerHeight;
+        var revealtop =  reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if(revealtop < windowheight - revealpoint){
+            reveals[i].classList.add("active");
+        }
+        else{
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+//backtop
+window.addEventListener('scroll', ontop);
+var ontop = document.querySelector(".ontop");
+function ontop(){
+    var content = document.querySelector('body');
+    var windowheight = window.innerHeight;
+    var revealtop =  content.getBoundingClientRect().top;
+    var revealpoint = 2000;  
+    if(revealtop < windowheight - revealpoint){
+        document.querySelector(".ontop").style.display = "block";
+    }
+    else{
+        document.querySelector(".ontop").style.display = "none";
+    }
+} 
+
+window.addEventListener('scroll', reveal);  
+function reveal(){
+    var reveals = document.querySelectorAll(".reveal");
+    for(var i = 0 ; i < reveals.length ; i++){
+        var windowheight = window.innerHeight;
+        var revealtop =  reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if(revealtop < windowheight - revealpoint){
+            reveals[i].classList.add("active");
+        }
+        else{
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener('scroll', sticky);  
+function sticky(){
+    var header = document.querySelectorAll("header");
+    header.classList.add("sticky", window.scroolY>0 )
+   
+}
+
+
+
